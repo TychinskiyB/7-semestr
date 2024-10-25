48512d27a94243be110a87e91677bbae2ddcc8b0
TychinskiyB <125715738+TychinskiyB@users.noreply.github.com>
2024-10-25 20:50:08 +0700

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,1 +1,12 @@
+class DiscriminantTest(unittest.TestCase):
 
+    def test_positive_discriminant(self):
+        self.assertEqual(calculate_discriminant(1, -5, 6), 1)
+        self.assertEqual(calculate_discriminant(1, 4, 3), 4)
+
+    def test_negative_discriminant(self):
+        self.assertEqual(calculate_discriminant(1, 2, 3), -8)
+        self.assertEqual(calculate_discriminant(1, 1, 1), -3)
+
+if __name__ == '__main__':
+    unittest.main()
